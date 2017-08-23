15660dae14736787f47ecccb9b241faa395e32b4
lewis-fidlers <lewisfidlers@gmail.com>
2017-08-23 21:30:13 +0200

    Setup devise.

--- db/migrate/20170823185753_create_meeting_rooms.rb
+++ db/migrate/20170823185753_create_meeting_rooms.rb
@@ -1,7 +1,7 @@
 class CreateMeetingRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :meeting_rooms do |t|
-      t.int :number_of_persons
+      t.integer :number_of_persons
       t.string :name
 
       t.timestamps
